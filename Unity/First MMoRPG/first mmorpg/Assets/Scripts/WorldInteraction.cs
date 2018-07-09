26bb302b41b54a477c402fb207eeabed7e557527
lopyos <lopyos@users.noreply.github.com>
2018-07-09 21:46:32 +0100

    MMORPG
    Messing with unity

---
+++ Unity/First MMoRPG/first mmorpg/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AI;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour
+{
+    NavMeshAgent playerAgent;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    playerAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+	    {
+            GetInteraction();
+	    }
+
+
+	}
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit interactionInfo;
+
+        if (Physics.Raycast(interactionRay, out interactionInfo,Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable object")
+            {
+                Debug.Log("interactable interacted");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+}
